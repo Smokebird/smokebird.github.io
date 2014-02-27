c215f9940deef1ee5b193aa2c66f1183b4edd432
Smokebird <eplatts14@gmail.com>
2014-02-27 13:10:03 -0600

    try to get 5 to show up

--- PF.js
+++ PF.js
@@ -3,8 +3,8 @@ PFF = function(){
     var x =5
     console.log(x) /*only writes to javascript console not page"*/
     /*document.write(x) /*writes five but gets ride of everthing else on the screen*/
-    document.getElementById("temp1").appendChild(x)
-
-    return x
+    document.getElementById("temp1") /*append child does not work*/
+    x.innerHTML="x"
+    /*return x this may not be needed*/
 }
 

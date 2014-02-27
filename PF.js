65e35f36d73f52b097c2a9587bd40acb793c0abf
Smokebird <eplatts14@gmail.com>
2014-02-27 14:21:02 -0600

    get number enter into form to work

--- PF.js
+++ PF.js
@@ -1,6 +1,6 @@
 
 PFF = function(form){
-    var x = form
+    var x = form.inputbox.value
     console.log(x) /*only writes to javascript console not page"*/
     /*document.write(x) /*writes five but gets ride of everthing else on the screen*/
     y=document.getElementById("temp1") /*append child does not work*/

f2624f7db5b6778048d7842e2ab6b1ffdd5af992
Smokebird <eplatts14@gmail.com>
2014-02-27 14:13:23 -0600

    tyring to get form to work

--- PF.js
+++ PF.js
@@ -1,6 +1,6 @@
 
-PFF = function(){
-    var x =5
+PFF = function(form){
+    var x = form
     console.log(x) /*only writes to javascript console not page"*/
     /*document.write(x) /*writes five but gets ride of everthing else on the screen*/
     y=document.getElementById("temp1") /*append child does not work*/

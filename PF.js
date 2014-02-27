ba70ea32f299b797cd389067b91433467c927437
Smokebird <eplatts14@gmail.com>
2014-02-27 13:33:17 -0600

    got letter to show now get number to show

--- PF.js
+++ PF.js
@@ -4,7 +4,7 @@ PFF = function(){
     console.log(x) /*only writes to javascript console not page"*/
     /*document.write(x) /*writes five but gets ride of everthing else on the screen*/
     y=document.getElementById("temp1") /*append child does not work*/
-    y.innerHTML="x"
+    y.innerHTML=x
     /*return x this may not be needed*/
 }
 

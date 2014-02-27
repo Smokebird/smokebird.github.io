f0f35b3202dbc52920c89dd71081f49d8c6a6e02
Smokebird <eplatts14@gmail.com>
2014-02-27 13:06:33 -0600

    askl;dfj

--- PF.js
+++ PF.js
@@ -3,7 +3,7 @@ PFF = function(){
     var x =5
     console.log(x) /*only writes to javascript console not page"*/
     /*document.write(x) /*writes five but gets ride of everthing else on the screen*/
-    document.getElementById("temp1")
+    document.getElementById("temp1").appendChild(x)
 
     return x
 }

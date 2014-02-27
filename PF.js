3b22bd104ffd600f1521329ec9aff36549b46d44
Smokebird <eplatts14@gmail.com>
2014-02-27 13:31:34 -0600

    git 5 to show up

--- PF.js
+++ PF.js
@@ -3,8 +3,8 @@ PFF = function(){
     var x =5
     console.log(x) /*only writes to javascript console not page"*/
     /*document.write(x) /*writes five but gets ride of everthing else on the screen*/
-    document.getElementById("temp1") /*append child does not work*/
-    x.innerHTML="x"
+    y=document.getElementById("temp1") /*append child does not work*/
+    y.innerHTML="x"
     /*return x this may not be needed*/
 }
 

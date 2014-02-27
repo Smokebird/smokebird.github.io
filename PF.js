974f758b1343f733ea0d33181971189b4eb3cd03
Smokebird <eplatts14@gmail.com>
2014-02-27 14:48:01 -0600

    checking out function

--- PF.js
+++ PF.js
@@ -1,6 +1,15 @@
 
 PFF = function(form){
     var x = form.inputbox.value
+    x = math.floor(x/2)
+    while(i<=x){
+	i = 2;
+	if(x%i==0){
+	    if(i%i=0){
+		y=i;
+	    }
+	}
+    }
     console.log(x) /*only writes to javascript console not page"*/
     /*document.write(x) /*writes five but gets ride of everthing else on the screen*/
     y=document.getElementById("temp1") /*append child does not work*/

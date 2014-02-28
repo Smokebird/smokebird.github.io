a7f9a8a6882d317fd73231bf7f9ee1f500c8e2e8
Smokebird <eplatts14@gmail.com>
2014-02-27 18:22:14 -0600

    fucntion checks to see if have remander of 0

--- PF.js
+++ PF.js
@@ -5,31 +5,11 @@ PFF = function(form){
     console.log(x)
    // var i = 2
     for(var i =2; i<x; i++){
-	console.log(i)
-	console.log(x)
-	if(x%i==0){
+	if(x%i){
+	    console.log(x)
 	    console.log(i)
-	   var  z=1;
-	    while(z<i){
-		console.log(z)
-		if(i%z==0){
-		    
-		    if(i==z){
-			console.log(z)
-			z=i;
-//			i=i++
-		    } // if i == z
-		} // if i%z
-		z=z++
-	    } // while z <i
-//	    i=i++
-	} //
-	else{
-//	i=i+1;
-	console.log(i)
 	}
-    } //while
-    console.log(z) /*only writes to javascript console not page"*/
+   // console.log(z) /*only writes to javascript console not page"*/
     /*document.write(x) /*writes five but gets ride of everthing else on the screen*/
     y=document.getElementById("temp1") /*append child does not work*/
     y.innerHTML=z

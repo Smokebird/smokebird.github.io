008416f11cb4a6e6aa8b84843307c844448e9f8c
Smokebird <eplatts14@gmail.com>
2014-02-27 20:01:08 -0600

    add x+1 to for loop

--- PF.js
+++ PF.js
@@ -3,7 +3,7 @@ PFF = function(form){
     var x = form.inputbox.value
     x = Math.floor(x/2);
     console.log(x)
-    for(var i =2; i<x; i++){
+    for(var i =2; i<x+1; i++){
 	if(x%i){
 	    console.log(x)
 	    console.log(i)
@@ -12,7 +12,7 @@ PFF = function(form){
    // console.log(z) /*only writes to javascript console not page"*/
     /*document.write(x) /*writes five but gets ride of everthing else on the screen*/
     y=document.getElementById("temp1") /*append child does not work*/
-    y.innerHTML=x
+    y.innerHTML=i
     /*return x this may not be needed*/
 }
 

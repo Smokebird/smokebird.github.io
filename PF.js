57fdfd1de0df068013fed45f726413335a7d3aa3
Smokebird <eplatts14@gmail.com>
2014-02-27 18:05:19 -0600

    get function to work

--- PF.js
+++ PF.js
@@ -7,11 +7,14 @@ PFF = function(form){
     while(i<x){
 	console.log(i)
 	if(x%i==0){
-	    
+	    console.log(i)
 	    z=1;
 	    while(z<i){
+		console.log(z)
 		if(i%z==0){
+		    
 		    if(i==z){
+			console.log(z)
 			z=i;
 			i=i++
 		    } // if i == z
@@ -20,7 +23,10 @@ PFF = function(form){
 	    } // while z <i
 	    i=i++
 	} //
+	else{
 	i=i++;
+	console.log(i)
+	}
     } //while
     console.log(z) /*only writes to javascript console not page"*/
     /*document.write(x) /*writes five but gets ride of everthing else on the screen*/

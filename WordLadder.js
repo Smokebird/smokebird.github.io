b40946c337b530693c100f4101a298dfce1d99dd
Emily <eplatts14@gmail.com>
2014-03-07 19:38:14 -0600

    conlose of first and last word

--- WordLadder.js
+++ WordLadder.js
@@ -1,2 +1,8 @@
 /*this is a JS file*/
 
+WLF = function(){
+    var firstWord = form.inputbox1.value
+    var lastWord = form.inputbox2.value
+    console.log(firstWord)
+    console.log(lastWord)
+}

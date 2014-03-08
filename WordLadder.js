d233307f3be89304142cd181d0ad60cb7eb57417
Emily <eplatts14@gmail.com>
2014-03-08 00:17:17 -0600

    have varbles to keep track of lenght of the words

--- WordLadder.js
+++ WordLadder.js
@@ -3,8 +3,13 @@
 WLF = function(form){
     var firstWord = form.inputbox1.value
     var lastWord = form.inputbox2.value
+    var FWwc  = firstWord.lenght;
+    console.log(FWwc)
+    var LWwc = lastWord.lenght
+    console.log(LWwc)
     console.log(firstWord)
     console.log(lastWord)
+    
 
     var y
     y=document.getElementById("temp1")

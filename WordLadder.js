c7dcfa2ed6146dcecb4a3076fc1517dd36f70d81
Emily <eplatts14@gmail.com>
2014-03-07 19:42:56 -0600

    put inputbox instead of inputbox1 to see if that makes a differen

--- WordLadder.js
+++ WordLadder.js
@@ -1,7 +1,7 @@
 /*this is a JS file*/
 
 WLF = function(){
-    var firstWord = form.inputbox1.value
+    var firstWord = form.inputbox.value
     var lastWord = form.inputbox2.value
     console.log(firstWord)
     console.log(lastWord)

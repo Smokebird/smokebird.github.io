f6a6baecf1667b0ef8416f3ae4068a6e534ba60f
Emily <eplatts14@gmail.com>
2014-03-08 18:26:53 -0600

    checking to see if the two words are of equal lenghts

--- WordLadder.js
+++ WordLadder.js
@@ -1,26 +1,24 @@
 /*this is a JS file*/
 
 WLF = function(form){
-    var firstWord = String(form.inputbox1.value) // adding quets to eat only returns what is in the quates
+    var y
+    var firstWord = (form.inputbox1.value) 
     var lastWord = form.inputbox2.value
     console.log(typeof firstWord)
     console.log(typeof lastWord)
-   // var FWwc = 0 this does not work still is undefined
-    FWwc  = Number(firstWord.length)// this does not work returns Nan not a real number ;
-    console.log(FWwc)
-    console.log(typeof FWwc)
+    var FWwc  = (firstWord.length)
     var LWwc = lastWord.length
-    console.log(LWwc)
-    console.log(typeof LWwc)
-    console.log(firstWord)
-    console.log(lastWord)
-    
-
-    var y
-    y=document.getElementById("temp1")
-    y.innerHTML=firstWord
-    var x
+    if (FWwc == LWwc){
+	console.log("same lenght")
+	}
+    else{
+	console.log("not same lenght")
+	 var y = "Please choose two words of equal langth."
+	y=document.getElementById("temp1")
+	y.innerHTML=firstWord
+	}
+   /* var x
     x=document.getElementById("temp2")
     x.innerHTML=lastWord
-			
+	*/		
 }

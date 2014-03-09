9c64dac1b659f671117118e21a222f152e316d13
Emily <eplatts14@gmail.com>
2014-03-08 18:02:44 -0600

    try a string methode on one text enter

--- WordLadder.js
+++ WordLadder.js
@@ -1,7 +1,7 @@
 /*this is a JS file*/
 
 WLF = function(form){
-    var firstWord = form.inputbox1.value // adding quets to eat only returns what is in the quates
+    var firstWord = string(form.inputbox1.value) // adding quets to eat only returns what is in the quates
     var lastWord = form.inputbox2.value
    // var FWwc = 0 this does not work still is undefined
     FWwc  = Number(firstWord.lenght)// this does not work returns Nan not a real number ;

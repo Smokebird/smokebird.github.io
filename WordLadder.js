7bbb03577520c4f2b06f2840185d693df637e210
Emily <eplatts14@gmail.com>
2014-03-12 22:59:56 -0500

    fixed typo

--- WordLadder.js
+++ WordLadder.js
@@ -16,7 +16,7 @@ WLF = function(form){
     var lastWord = form.inputbox2.value
     var numLetters =form.numLetters.value
     var numLetters = Number(numLetters)
-    console.log(z.push(firstWord)
+    console.log(z.push(firstWord))
     var FWwc  = (firstWord.length)
     var LWwc = lastWord.length
     if (FWwc == LWwc){

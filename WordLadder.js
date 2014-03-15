d28e60cb6cefd1727fe90d37fa98af8f41da97b4
Emily <eplatts14@gmail.com>
2014-03-14 23:49:51 -0500

    see what e is

--- WordLadder.js
+++ WordLadder.js
@@ -97,6 +97,7 @@ WLF = function(form){
 			    for(var j=0; j< 2; j++){
 				console.log(j)
 				e=f.pop()
+				console.log(e)
 				var errorcount = 0
 				if(e.substring(j,j+1) != threeLetterWords[i].substring(j,j+1)){
 				    errorcount=errorcount+1

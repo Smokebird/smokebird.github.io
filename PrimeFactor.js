08f2a94e70491b040b85662a8bbe1d6dd2e55ed8
Emily <eplatts14@gmail.com>
2015-06-16 15:54:30 -0500

    the correct output is fiven

--- PrimeFactor.js
+++ PrimeFactor.js
@@ -1,8 +1,24 @@
 
 function PrimeFactor(form){
     var Num = parseInt(form.number.value);
-    console.log(Num);
+    var Primes = [];
     
+    while (Num % 2 == 0){
+	Num = Num / 2;
+	Primes.push(2)
+	}
+    while (Num % 3 == 0){
+	Num = Num / 3;
+	Primes.push(3)
+	}
+    while(Num % 5 == 0){
+	Num = Num / 5;
+	Primes.push(5)
+	}
+    while(Num % 7 == 0){
+	Num = Num / 7;
+	Primes.push(7)
+	}
+    document.getElementById("result").innerHTML = Primes;
+
 }
-
-

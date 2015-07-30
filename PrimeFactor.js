30c889cfd38ae03ebd0ed64f6da9bad761967a5c
Emily <eplatts14@gmail.com>
2015-07-30 05:41:07 -0500

    having issue with angulerJs switching to different project

--- PrimeFactor.js
+++ PrimeFactor.js
@@ -1,3 +1,4 @@
+"use strick";
 
 function PrimeFactor(form){
     var Num = parseInt(form.number.value);
@@ -22,6 +23,42 @@ function PrimeFactor(form){
     if (Num != 1 && Num % 1 == 0){
 	Primes.push(Num)
 	}
-    document.getElementById("result").innerHTML = Primes;
+        var outPutType = document.getElementsByName("output");
+    if (outPutType[0].checked == true){
+	// output is an unorder list
+	document.getElementById("result").innerHTML = Primes;
+	}
+    else if (outPutType[1].checked == true){
+	//output is an unorder table
+	//var table = document.createElement("table") ;
+	//var tHead = document.createElement('thead');
+	//var heading = document.createElement('tr');
+	//heading.appendChild(document.createTextNode("Prime Numbers"));
+	//document.body.appendChild(table)
+	//document.body.appendChild(tHead)
+	//document.body.appendChild(heading)
+	//var tbody = document.createElement('tbody');
+	//for (num in Primes){
+	    
+	  //  var row = document.createElement('tr');
+	    //row.appendChild(document.createTextNode(num))
+	    //document.body.appendChild(row)
+	  //  }
+	var myApp = angular.module('tableCreator', []);
+	myApp.controller('table', ['$scope' , function($scope) {
+	    $scope.a = 'hello';
+	    $scope.b = 'world';
+	    $scope.primes = [1,2,3]
+	  }]);
+	//document.getElementById("result").innerHTML = "this output is not supported yet"
+	}
+    else if (outPutType[2].checked == true){
+	// output is exponational form
+	document.getElementById("result").innerHTML = "this output is not supported yet"
+	}
+    else if (outPutType[3].checked == true){
+	// output is a factor tree
+	document.getElementById("result").innerHTML = "this output is not supported yet"
+	}
+};
 
-}
